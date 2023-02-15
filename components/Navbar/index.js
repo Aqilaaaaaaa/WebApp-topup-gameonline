9fc877f7b3442c3f6378ba58e745c36c8d0447aa
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-02-16 03:24:17 +0700

    add component nav button icon

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import Image from 'next/image';
-import fas from '@fortawesome/free-solid-svg-icons'
-FiSearch
+import Image from 'next/image'; 
+import { useState } from "react";
+import { BsSearch, BsHouseFill } from "react-icons/bs";
 
 
-export default function Index(props){
+export default function Index(){
+
+    const [show, setShow] = useState(false) 
+    const handleShow = () => {
+        setShow(false)
+    } ;
     // const getNavLinkClass = (path) => {
     //     return props.location.pathname === path ? " active" : "";
     //   };
@@ -15,7 +20,7 @@ export default function Index(props){
                 <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/#">
-                            <Image src="icon/logo.svg" width={60} height={60}/>
+                            <Image src="icon/logo.svg" width={80} height={80}/>
                         </a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,13 +28,27 @@ export default function Index(props){
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
-                                <div>
-                                    <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                                    
-                                </div>
-                                <li className="nav-item">
-                                    <Link className="nav-link" type="link" href="/">
-                                        Home
+                                <li className="nav-item d-flex">
+                                    <form className="d-flex  rounded" action="action_page.php">
+                                    {show ?(
+                                        <input type="search" 
+                                        className="form-control rounded border"  
+                                        placeholder="Search.." 
+                                        name="search"
+                                        onClick={() => setShow(true)}
+                                        />
+
+                                    ):(
+                                        <button 
+                                        className="border border-light bg-transparent"
+                                        onClick={() => setShow(true)}>
+                                            <BsSearch />
+                                        </button>
+
+                                    )}
+                                    </form>
+                                    <Link className="nav-link w-6 h-6" type="link" href="/">
+                                        <BsHouseFill/>
                                     </Link>
                                 </li>
                                 <li className="nav-item my-auto">
