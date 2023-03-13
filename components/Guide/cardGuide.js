9a055170a4e2d73d4aacf947ed15000a50fa4fd1
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-03-13 22:33:23 +0700

    add bg checkout

--- components/Guide/cardGuide.js
+++ components/Guide/cardGuide.js
@@ -6,7 +6,7 @@ const CardGuide = ({icon, title, desc}) => {
        <div className="col-lg-4">
             <div className="card feature-card border-0">
                 <img src={icon} width={80} height={80} />
-                <p className="fw-semibold text-2xl mb-2 color-palette-1">{title}</p>
+                <p className="fw-semibold text-2xl mb-2 mt-4 color-palette-1 ">{title}</p>
                 <p className="text-lg color-palette-1 mb-0">{desc}</p>
             </div>
         </div>
