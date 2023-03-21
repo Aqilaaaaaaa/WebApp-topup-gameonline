a475d27ff2118a005fdddd72a4a723ea81e05d54
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-03-21 15:38:18 +0700

    add script

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -28,6 +29,11 @@ export default function Document() {
           crossOrigin="anonymous"
         />
       </Head>
+      <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+          crossOrigin="anonymous"
+        />
       <body>
         <Main />
         <NextScript />
