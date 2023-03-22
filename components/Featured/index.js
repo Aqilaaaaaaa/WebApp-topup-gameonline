e1497c495cd4246d42eb2a96fef556d93aad5d1b
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-03-22 23:41:54 +0700

    add swiperSlide on featured (2)

--- components/Featured/index.js
+++ components/Featured/index.js
@@ -1,25 +1,82 @@
 import CardFeatured from "./cardFeatured";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import React, { useCallback, useRef } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default function Featured(){
+
+const Featured = () => {
+    const swiperRef = useRef();
+    const handlePrev = useCallback(() => {
+        if (!sliderRef.current) return;
+        sliderRef.current.swiper.slidePrev();
+      }, []);
+    
+    const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+    }, []);
+
     return (
         <>
             <section className="featured-game pt-50 pb-50">
                 <div className="container-fluid">
                     <h2 className="text-4xl fw-bold color-palette-1 mb-30">Our Featured Games</h2>
-                    <div className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
+                    <div className="container overflow-setting justify-content-lg-between gap-lg-3 gap-4"
                         data-aos="fade-up">
-                        <button className="pre-button border-0 bg-transparent"><IoIosArrowBack size={30}/></button>
-                        <CardFeatured img="Thumbnail-1" title="Super Mechs" type="Desktop"/>
-                        <CardFeatured img="Thumbnail-2" title="Mobile Legends" type="Mobile"/>
-                        <CardFeatured img="Thumbnail-3" title="Clash of Clans" type="Mobile"/>
-                        <CardFeatured img="Thumbnail-4" title="Mobile Legends" type="Mobile"/>
-                        {/* <CardFeatured img="Thumbnail-1" title="Mobile Legends" type="Mobile"/>
-                        <CardFeatured img="Thumbnail-5" title="Mobile Legends" type="Mobile"/> */}
-                        <button className="nxt-button border-0 bg-transparent"><IoIosArrowForward size={30}/></button>
+                        {/* <div className="container-btnR" >
+                            <button className="pre-button border-0 bg-transparent" onClick={() => swiperRef.current?.slidePrev()}><IoIosArrowBack size={30}/></button>
+                        </div> */}
+                        <Swiper
+                            className=""
+                            breakpoints={
+                                {
+                                1280: {
+                                    slidesPerView: 3,
+                                    width: 800,
+                                },
+                                768: {
+                                    slidesPerView: 2,
+                                    width: 760,
+                                    spaceBetween: 50
+                                },
+                                414: {
+                                    slidesPerView: 1,
+                                    width: 400,
+                                },
+                                360:{
+                                    slidesPerView: 1,
+                                    width: 360,
+                                }
+                                }
+                            }
+                            slidesPerView={4}
+                            onBeforeInit={(swiper) => {
+                                swiperRef.current = swiper;
+                            }}
+                        >
+                            
+                            <SwiperSlide className="m-auto p-4" >
+                                <CardFeatured img="Thumbnail-1" title="Super Mechs" type="Desktop"/>
+                            </SwiperSlide>
+                            <SwiperSlide className="m-auto p-4" >
+                                <CardFeatured img="Thumbnail-1" title="Super Mechs" type="Desktop"/>
+                            </SwiperSlide>
+                            <SwiperSlide className="m-auto p-4" >
+                                <CardFeatured img="Thumbnail-1" title="Super Mechs" type="Desktop"/>
+                            </SwiperSlide>
+                            <SwiperSlide className="m-auto p-4" >
+                                <CardFeatured img="Thumbnail-1" title="Super Mechs" type="Desktop"/>
+                            </SwiperSlide>
+                                
+                        </Swiper>
+                        {/* <div className="container-btnL">
+                            <button className="nxt-button border-0 bg-transparent" onClick={() => swiperRef.current?.slideNext()}><IoIosArrowForward size={30}/></button>
+                        </div> */}
                     </div>
                 </div>
             </section>
+            
         </>
     )
 }
+export default Featured;
