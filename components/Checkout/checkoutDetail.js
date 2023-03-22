a6d6aef57fbc41dca2e2eb5c5e384c18aeb08841
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-03-22 23:19:33 +0700

    add components Checkout

--- components/Checkout/checkoutDetail.js
+++ components/Checkout/checkoutDetail.js
@@ -5,8 +5,9 @@ const CheckoutDetail = () => {
         <>
             <div className="purchase pt-md-50 pt-30">
                 <h2 className="fw-bold text-xl color-palette-1 mb-20">Purchase Details</h2>
-                <p className="text-lg color-palette-1 mb-20">Your Game ID <span
-                        className="purchase-details">masayoshizero</span></p>
+                <p className="text-lg color-palette-1 mb-20">Your Game ID 
+                        <span className="purchase-details">masayoshizero</span>
+                </p>
                 <p className="text-lg color-palette-1 mb-20">Order ID <span className="purchase-details">#GG001</span></p>
                 <p className="text-lg color-palette-1 mb-20">Item <span className="purchase-details">250 Diamonds</span></p>
                 <p className="text-lg color-palette-1 mb-20">Price <span className="purchase-details">Rp 42.280.500</span></p>
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,15 +1,16 @@
 import CheckoutDetail from "@/components/Checkout/checkoutDetail";
 import CheckoutInform from "@/components/Checkout/checkoutInform";
 import CheckoutItem from "@/components/Checkout/checkoutItem";
+import Link from "next/link";
 
 export default function Checkout() {
     return (
         <section className="checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30">
             <div className="container-fluid">
                 <div className="logo text-md-center text-start pb-50">
-                    <a className="" href="/#">
-                        <img src="icon/logoo.png" width={130} height={130}/>
-                    </a>
+                    <Link href="/#">
+                        <img src="icon/logoo.png" className="logo" width={130} height={130}/>
+                    </Link>
                 </div>
                 <div className="title-text pt-md-50 pt-0">
                     <h2 className="text-4xl fw-bold color-palette-1 mb-10">Checkout</h2>
