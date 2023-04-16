0dc6d61638c8bd463c8d64831af191a355fa75ed
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-04-16 11:35:51 +0700

    Product Page : add pagination

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 export const products = [
     {
         id :1,
-        name : "Mobile Legend",
+        name : "MobileLegend",
         img :'/img/Thumbnail-5.png',
         type : "Mobile",
         rate : 5,
@@ -238,7 +238,7 @@ export const products = [
         ]
     },
     {
-        id :7,
+        id :8,
         name : "Valorant",
         img :'/img/Thumbnail-3.jpg',
         type : "PC",
@@ -272,6 +272,436 @@ export const products = [
                 price : 240000,
                 coin : 2
             }
+        ]
+    },
+    {
+        id :9,
+        name : "MobileLegend",
+        img :'/img/Thumbnail-5.png',
+        type : "Mobile",
+        rate : 5,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Diamond",
+                price : 10000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Diamond",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Diamond",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"40",
+                itemName : "Diamond",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 5,
+                item :"40",
+                itemName : "Diamond",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :10,
+        name : "Clash Of Clan",
+        img :'/img/Thumbnail-4.png',
+        type : "Mobile",
+        rate : 3,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Gems",
+                price : 10000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Gems",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Gems",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"80",
+                itemName : "Gems",
+                price : 40000,
+                coin : 3
+            },
+        ]
+    },
+    {
+        id :11,
+        name : "Modern Warfare",
+        img :'/img/Thumbnail-2.png',
+        type : "PC",
+        rate : 4,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "CP",
+                price : 10000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "CP",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "CP",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :12,
+        name : "Super Mecha",
+        img :'/img/Thumbnail-1.png',
+        type : "Mobile",
+        rate : 2,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Energy Crystal",
+                price : 10000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Energy Crystal",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Energy Crystal",
+                price : 240000,
+                coin : 3
+            }
+        ]
+    },
+    {
+        id :13,
+        name : "Sausage Man",
+        img :'/img/Thumbnail-6.jpg',
+        type : "Mobile",
+        rate : 4,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Candy",
+                price : 1000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Candy",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Candy",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :14,
+        name : "Wild Trift",
+        img :'/img/Thumbnail-7.jpg',
+        type : "Mobile",
+        rate : 5,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Wild Cores",
+                price : 1000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Wild Cores",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Wild Cores",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :15,
+        name : "Dota 2",
+        img :'/img/Thumbnail-8.jpg',
+        type : "PC",
+        rate : 3,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Steam Wallet",
+                price : 1000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Steam Wallet",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Steam Wallet",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"40",
+                itemName : "Steam Wallet",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :16,
+        name : "Valorant",
+        img :'/img/Thumbnail-3.jpg',
+        type : "PC",
+        rate : 5,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Points",
+                price : 1000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Points",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Points",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"40",
+                itemName : "Points",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :17,
+        name : "Dota 2",
+        img :'/img/Thumbnail-8.jpg',
+        type : "PC",
+        rate : 3,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Steam Wallet",
+                price : 1000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Steam Wallet",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Steam Wallet",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"40",
+                itemName : "Steam Wallet",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :18,
+        name : "Valorant",
+        img :'/img/Thumbnail-3.jpg',
+        type : "PC",
+        rate : 5,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Points",
+                price : 1000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Points",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Points",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"40",
+                itemName : "Points",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :19,
+        name : "MobileLegend",
+        img :'/img/Thumbnail-5.png',
+        type : "Mobile",
+        rate : 5,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Diamond",
+                price : 10000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Diamond",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Diamond",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"40",
+                itemName : "Diamond",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 5,
+                item :"40",
+                itemName : "Diamond",
+                price : 240000,
+                coin : 2
+            }
+        ]
+    },
+    {
+        id :20,
+        name : "Lorem ipsum",
+        img :'/img/Thumbnail-4.png',
+        type : "Mobile",
+        rate : 3,
+        priceList: [
+            {
+                idProduct : 1,
+                item :"20",
+                itemName : "Gems",
+                price : 10000,
+                coin : 1
+            },
+            {
+                idProduct : 2,
+                item :"50",
+                itemName : "Gems",
+                price : 27000,
+                coin : 3
+            },
+            {
+                idProduct : 3,
+                item :"40",
+                itemName : "Gems",
+                price : 240000,
+                coin : 2
+            },
+            {
+                idProduct : 4,
+                item :"80",
+                itemName : "Gems",
+                price : 40000,
+                coin : 3
+            },
         ]
     }
 ]
