6073636c4f9518444b78f972ad53ee5c68a9a0ad
HAQILA NUR NOUVADILA <haqilaan@gmail.com>
2023-05-26 23:14:27 +0700

    Navbar fixed

--- components/Navbar/Auth.js
+++ components/Navbar/Auth.js
@@ -31,39 +31,54 @@ export default function Auth(){
     }
     console.log(logOut)
 
-    if (isLogin) {
-        return (
-            <li className="nav-item my-auto dropdown d-flex">
-                <div className="vertical-line d-lg-block d-none"></div>
-                <div>
-                    <Link className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <Image src="/img/avatar-1.png" 
-                            className="rounded-circle" width="40" height="40"
-                           alt=""/>
-                    </Link>
+    // if (isLogin) {
+    //     return (
+    //         <li className="nav-item my-auto dropdown d-flex">
+    //             <div className="vertical-line d-lg-block d-none"></div>
+    //             <div>
+    //                 <Link className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink"
+    //                     data-bs-toggle="dropdown" aria-expanded="false">
+    //                     <Image src="/img/avatar-1.png" 
+    //                         className="rounded-circle" width="40" height="40"
+    //                        alt=""/>
+    //                 </Link>
 
-                    <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
-                        <li><Link href="/historyPurchase" className="dropdown-item text-lg color-palette-2">Cash Coin : Rp 15.000</Link></li>
-                        <li><Link href="/historyPurchase" className="dropdown-item text-lg color-palette-2">History Purchase</Link></li>
-                        <li onClick={logOut} className="dropdown-item text-lg color-palette-2"> Log Out</li>
-                    </ul>
-                </div>
-            </li>   
-        );
-    }
+    //                 <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
+    //                     <li><Link href="/historyPurchase" className="dropdown-item text-lg color-palette-2">Cash Coin : Rp 15.000</Link></li>
+    //                     <li><Link href="/historyPurchase" className="dropdown-item text-lg color-palette-2">History Purchase</Link></li>
+    //                     <li onClick={logOut} className="dropdown-item text-lg color-palette-2"> Log Out</li>
+    //                 </ul>
+    //             </div>
+    //         </li>   
+    //     );
+    // }
+    // return (
+    //     <>
+    //         <li className="nav-item my-auto">
+    //             <Link className="btn btn-login d-flex justify-content-center ms-lg-2 rounded-pill"
+    //                 href="/account/login" role="button"> Sign In
+    //             </Link>
+    //             {/* <li onClick={logOut} className="dropdown-item text-lg color-palette-2"> Log Out</li> */}
+    //         </li>
+    //     </> 
+    // );
     return (
-        <>
-            <li className="nav-item my-auto">
-                <Link className="btn btn-login d-flex justify-content-center ms-lg-2 rounded-pill"
-                    href="/account/login" role="button"> Sign In
+        <li className="nav-item my-auto dropdown d-flex">
+            <div className="vertical-line d-lg-block d-none"></div>
+            <div>
+                <Link className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <Image src="/img/avatar-1.png" 
+                        className="rounded-circle" width="40" height="40"
+                       alt=""/>
                 </Link>
-                <li onClick={logOut} className="dropdown-item text-lg color-palette-2"> Log Out</li>
-            </li>
-        </> 
-        
-        
-                         
-    
-);
+
+                <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
+                    <li><Link href="/historyPurchase" className="dropdown-item text-lg color-palette-2">Cash Coin : Rp 15.000</Link></li>
+                    <li><Link href="/historyPurchase" className="dropdown-item text-lg color-palette-2">History Purchase</Link></li>
+                    <li onClick={logOut} className="dropdown-item text-lg color-palette-2"> Log Out</li>
+                </ul>
+            </div>
+        </li>   
+    );
 }
